b040f47e486e493d47a9c7737577f9ed60ff04c7
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-15 21:06:00 +0600

    execution priority

---
+++ 7/priority.js
@@ -0,0 +1,8 @@
+const tigerMass = 600
+const elephantMass = 1000
+
+// There are several compare operators (<, >, >=, <=, ==)
+const elephantIsBigger = elephantMass - tigerMass >= 0
+console.log(elephantIsBigger)
+
+// Operators' prioritization https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence

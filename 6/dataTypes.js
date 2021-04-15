e8fdbccbb4a56604e99537b56acf9ec6ee025a3d
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-15 21:05:51 +0600

    data types + typeof

---
+++ 6/dataTypes.js
@@ -0,0 +1,12 @@
+const month = 'February'
+const isWinter = true
+const days = 28
+let x
+// const x <- // Error
+
+console.log(typeof month)       // string
+console.log(typeof isWinter)    // boolean
+console.log(typeof days)        // number
+console.log(typeof x)           // undefined
+console.log(typeof null)        // object ? (should be `null`)
+

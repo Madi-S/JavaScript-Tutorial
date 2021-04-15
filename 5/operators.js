7c64bc8efe2b433f2991002c2ec3758dbe9cfa5b
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-15 21:05:40 +0600

    operators + = - /

---
+++ 5/operators.js
@@ -0,0 +1,22 @@
+
+const age = Number(prompt('Enter your age:'))
+const currentYer = 2021
+
+let birthYear1 = 2021 - age
+let birthYear2 = 2021 - age - 1
+
+birthYear1 ++
+birthYear2 --
+
+alert('You were born on ' + --birthYear1 + ' or ' + ++birthYear2)
+
+let a = 25
+console.log(a) 
+a -= 5
+console.log(a) 
+a += 25
+console.log(a) 
+a *= 3
+console.log(a) 
+a /= 17
+console.log(a)
